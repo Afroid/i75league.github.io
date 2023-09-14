c0e03e645faecdf0c00417e23dd390fe057a382b
The John <jsqueet08@gmail.com>
2023-09-13 23:47:33 -0400

    Pushing a small change to test it out.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         09/13/2023 Recap
+        </p>
+        <p>
+         This is where the recap goes.
         </p>
         <a
           className="App-link"
